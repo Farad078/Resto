9d7c38b6ef7e8ac105cefc1565b9555ad3995e6e
Farad078 <faradokorie@gmail.com>
2023-06-18 04:29:24 +0100

    pyc files removed

--- UI/__init__.py
+++ UI/__init__.py
@@ -3,5 +3,6 @@ import os, sys
 path = os.getcwd()
 sys.path.append(path)
 
-from UI.card.card import HeroCard
+# from UI.card.card import HeroCard
 from Backend.apiconnect import ApiConnect
+from Backend import creds
